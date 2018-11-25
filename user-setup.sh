c87a777102bd42351fa94ead338be83e10c516a3
Kyle Buzby <kyle@kylebuzby.com>
2018-11-25 17:49:10 -0600

    Update user-setup.sh

--- user-setup.sh
+++ user-setup.sh
@@ -21,5 +21,15 @@ echo "Checking out setup scripts"
 mkdir -p ~/contrib
 git clone https://github.com/kbuzby/kvm-install-vm.git ~/contrib/kvm-install-vm
 
+echo "Starting guests"
+pushd ~/contrib/kvm-install-vm
+
+echo "Starting MalOS"
+sudo ./kvm-install-vm create -m 8192 -t ubuntu1804 malguest
+
+echo "Starting innocents"
+sudo ./kvm-install-vm create -m 8192 -t ubuntu1804 ubuntu1
+popd
+
 echo "Done!"
 
